767ed00ff1285faef8d70dc33555d8481eabf22f
Vishal <vishal.sharma@in.pega.com>
2024-09-02 13:58:15 +0530

    Fix Prettier config error

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+// Prettier config options: https://prettier.io/docs/en/options.html
+// Shared front-end config: https://git.pega.io/projects/FE/repos/configs/browse/packages/prettier-config/index.json
+
+import pegaPrettierConfig from '@pega/prettier-config';
+
+export default {
+  ...pegaPrettierConfig,
+  printWidth: 150
+};
