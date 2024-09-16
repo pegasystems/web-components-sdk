4a3070afcde5102214f2a21eae698ecba4ee2d8d
mashm <manasa.mashetty@in.pega.com>
2024-09-16 19:42:37 +0530

    Fixed the issues identified while internal adoption testing

--- src/components/forms/Percentage/index.ts
+++ src/components/forms/Percentage/index.ts
@@ -1,5 +1,5 @@
 import { html, nothing } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 import { FormComponentBase } from '../FormComponentBase';
 
 // NOTE: you need to import ANY component you may render.
