3dfa8c72063c6858c79722b4d5ad898e85364db4
Vishal <vishal.sharma@in.pega.com>
2024-09-26 16:27:33 +0530

    Pick last commit

---
+++ src/components/forms/Decimal/decimal-styles.ts
@@ -0,0 +1,9 @@
+import { html } from 'lit';
+
+export const decimalStyles = html`
+  <style>
+    input {
+      text-align: left;
+    }
+  </style>
+`;
---
+++ src/components/forms/Percentage/percentage-styles.ts
@@ -0,0 +1,9 @@
+import { html } from 'lit';
+
+export const percentageStyles = html`
+  <style>
+    input {
+      text-align: left;
+    }
+  </style>
+`;
