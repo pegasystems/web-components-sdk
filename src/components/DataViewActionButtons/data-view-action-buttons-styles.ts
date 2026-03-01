a6ef38db48e8a1e538b205cd5810d392db12fb2e
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2026-03-01 22:52:48 +0530

    fix: Issue with showDataObjectCreateView API in modal

---
+++ src/components/DataViewActionButtons/data-view-action-buttons-styles.ts
@@ -0,0 +1,14 @@
+import { css } from 'lit';
+
+export const dataViewActionButtonsStyles = css`
+  .div {
+    display: flex;
+    justify-content: space-between;
+    margin: 8px;
+  }
+
+  .button {
+    margin: 4px;
+    height: 40px;
+  }
+`;
