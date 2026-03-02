cdb1fd4d83ee3cb02c2e73c05b95c8268f2c7296
samhere06 <sameeryamdi@gmail.com>
2026-03-02 15:23:05 +0530

    prettier change

--- src/components/fields/Dropdown/index.ts
+++ src/components/fields/Dropdown/index.ts
@@ -291,7 +291,11 @@ class Dropdown extends FormComponentBase {
                     id=${this.theComponentId}
                     dataTestId=${this.testId}
                     .fieldName=${this.label}
-                    .modelValue=${this.thePConn.getLocalizedValue(this.value === '' && !this.bReadonly ? 'Select' : this.value, this.localePath, this.thePConn.getLocaleRuleNameFromKeys(this.localeClass, this.localeContext, this.localeName))}
+                    .modelValue=${this.thePConn.getLocalizedValue(
+                      this.value === '' && !this.bReadonly ? 'Select' : this.value,
+                      this.localePath,
+                      this.thePConn.getLocaleRuleNameFromKeys(this.localeClass, this.localeContext, this.localeName)
+                    )}
                     .validators=${this.lionValidatorsArray}
                     .feedbackCondition=${this.requiredFeedbackCondition.bind(this)}
                     ?readonly=${this.bReadonly}
